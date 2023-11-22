07e0058ce63eb6366d43c28318286d0cc1d605f2
manfrommother <mr.lipova@list.ru>
2023-11-22 17:01:42 +0300

    Lern new library datatime and done some tasks

---
+++ data_time/func_get_min_max_date.py
@@ -0,0 +1,20 @@
+'''
+Условие довольно долгое, поэтому напишу его своим языком. Надо написать функцию,
+в которую передается список, функция должна вернуть кортеж из минимальной и максимальной даты
+'''
+
+
+from datetime import date
+
+
+def get_min_max_date(dates):
+
+    if len(dates) == 0:
+        return ()
+    
+    return min(dates), max(dates)
+
+
+dates = []
+
+print(get_min_max_date(dates))
