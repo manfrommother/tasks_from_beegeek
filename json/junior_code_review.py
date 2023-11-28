4e4fb31e8ba362189db7b4fd04d753b2dad06047
manfrommother <mr.lipova@list.ru>
2023-11-28 19:30:26 +0300

    Corrected task with code-review

--- json/junior_code_review.py
+++ json/junior_code_review.py
@@ -1,3 +1,9 @@
+'''
+Надо исправить ошибку в коде, чтобы кириллица не заменялась на кодировку
+'''
+#старый код
+
+'''
 import json
 
 specs = {
@@ -11,8 +17,24 @@ specs = {
          'Базовая частота': '3900 МГц'
         }
 
-specs_json = json.dumps(specs, ensure_ascii=False)
-#specs_json = json.load(specs_json)
+specs_json = json.dumps(specs, ensure_ascii=True)
+
+print(specs_json)
+'''
+import json
+
+specs = {
+         'Модель': 'AMD Ryzen 5 5600G',
+         'Год релиза': 2021,
+         'Сокет': 'AM4',
+         'Техпроцесс': '7 нм',
+         'Ядро': 'Cezanne',
+         'Объем кэша L2': '3 МБ',
+         'Объем кэша L3': '16 МБ',
+         'Базовая частота': '3900 МГц'
+        }
+
+specs_json = json.dumps(specs, ensure_ascii=False, indent=3)
 
 print(specs_json)
 
