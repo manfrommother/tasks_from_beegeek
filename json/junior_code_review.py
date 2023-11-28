197241f8459d78d49c3d469ac867278cbddc5110
manfrommother <mr.lipova@list.ru>
2023-11-28 19:25:40 +0300

    Passed tasks used skipkeys and minor debug code of another author

---
+++ json/junior_code_review.py
@@ -0,0 +1,18 @@
+import json
+
+specs = {
+         'Модель': 'AMD Ryzen 5 5600G',
+         'Год релиза': 2021,
+         'Сокет': 'AM4',
+         'Техпроцесс': '7 нм',
+         'Ядро': 'Cezanne',
+         'Объем кэша L2': '3 МБ',
+         'Объем кэша L3': '16 МБ',
+         'Базовая частота': '3900 МГц'
+        }
+
+specs_json = json.dumps(specs, ensure_ascii=False)
+#specs_json = json.load(specs_json)
+
+print(specs_json)
+
