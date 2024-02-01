54d4f5fd7c4082f1ac9a5edf515e21b1ab4a1a0e
manfrommother <mr.lipova@list.ru>
2024-02-01 18:04:08 +0300

    implement strip_range decorator

---
+++ decorator/dec_strip_range.py
@@ -0,0 +1,46 @@
+'''
+Реализуйте декоратор strip_range, который принимает три аргумента в следующем порядке:
+
+start — неотрицательное целое число
+end — неотрицательное целое число
+char — одиночный символ, по умолчанию равный точке .
+Декоратор должен изменять возвращаемое значение декорируемой функции, 
+заменяя все символы в диапазоне индексов от start (включительно) 
+до end (не включительно) на символ char.
+
+Также декоратор должен сохранять имя и строку документации декорируемой функции.
+'''
+
+import functools
+
+def strip_range(start, end, char='.'):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            total = func(*args, **kwargs)
+            total_str = ''
+            try:
+                for i in range(len(total)):
+                    if i < start or i >= end:
+                        total_str += total[i]
+                    elif i >= start and i < end:
+                        total_str += char
+                return total_str
+            except IndexError:
+                for i in range(len(total)):
+                    if i < start or i >= len(total) - 1:
+                        total_str += total[i]
+                    elif i >= start and i < len(total) - 1:
+                        total_str += char
+                return total_str
+        return wrapper
+    return decorator
+
+
+@strip_range(20, 30)
+def beegeek():
+    return 'beegeek'
+    
+print(beegeek())
+
+                     
