ed55403a0de41e76b6f1b3e34ed31c8b7174bdc7
manfrommother <mr.lipova@list.ru>
2024-02-05 19:42:47 +0300

    implement powerOf iterator

---
+++ iterators/iterator_powerOf.py
@@ -0,0 +1,28 @@
+'''
+Реализуйте класс PowerOf, порождающий итераторы, конструктор которого принимает один аргумент:
+
+number — ненулевое число
+Итератор класса PowerOf должен генерировать бесконечную последовательность целых неотрицательных степеней 
+числа number в порядке возрастания, начиная с нулевой степени.
+'''
+
+class PowerOf:
+
+    def __init__(self, number):
+        self.number = number
+        self.degree = -1
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self.degree += 1
+        return self.number ** self.degree
+
+
+power_of_two = PowerOf(2)
+
+print(next(power_of_two))
+print(next(power_of_two))
+print(next(power_of_two))
+print(next(power_of_two))
