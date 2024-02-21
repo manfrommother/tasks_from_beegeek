6f1ec37fe5b058fee66dc24590c3942c74376bdb
manfrommother <145666282+manfrommother@users.noreply.github.com>
2024-02-21 12:51:58 +0300

    Обновил файл class_hierarchy.py

---
+++ OOP/Наследование/class_hierarchy.py
@@ -0,0 +1,40 @@
+'''
+С помощью наследования и приведенной ниже схемы 
+постройте иерархию пустых классов, 
+описывающих транспортные средства:
+
+Vehicle
+--LandVehicle
+----Car
+----Motorcycle
+----Bicycle
+--WaterVehicle
+--AirVehicle
+----Propeller
+----Jet
+'''
+
+class Vehicle:
+    pass
+
+
+class LandVehicle(Vehicle):
+    pass
+class Car(LandVehicle):
+    pass
+class Motorcycle(LandVehicle):
+    pass
+class Bicycle(LandVehicle):
+    pass
+
+
+class WaterVehicle(Vehicle):
+    pass
+
+
+class AirVehicle(Vehicle):
+    pass
+class Propeller(AirVehicle):
+    pass
+class Jet(AirVehicle):
+    pass
